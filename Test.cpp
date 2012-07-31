8676cb314e9371cd80c45cb8d01dd25cdbc6d2f8
pmp0x <marcus.putz@gmail.com>
2012-07-31 17:20:33 +0200

    init

---
+++ Test.cpp
@@ -0,0 +1,14 @@
+//
+//  Test.cpp
+//  StudienarbeitX
+//
+//  Created by Marcus Putz on 7/31/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#include <iostream>
+
+void main(){
+    
+    return -1;
+}
